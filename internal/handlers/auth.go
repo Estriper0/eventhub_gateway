7a12ff603789b4a7421a888a3bb8126ac4f6bccb
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2025-11-05 16:57:40 +0300

    Добавил ендпоинты refresh и logout

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -112,14 +112,14 @@ func (a *Auth) Login(c *gin.Context) {
 		c.JSON(
 			http.StatusBadRequest,
 			gin.H{
-				"code":    http.StatusBadRequest,
-				"token":   nil,
-				"message": "JSON is incorrect",
+				"code":          http.StatusBadRequest,
+				"access_token":  nil,
+				"refresh_token": nil,
+				"message":       "JSON is incorrect",
 			},
 		)
 		return
 	}
-	req.AppId = int32(a.config.AppId)
 
 	resp, err := a.authClient.Login(ctx, &req)
 	if err != nil {
@@ -127,9 +127,10 @@ func (a *Auth) Login(c *gin.Context) {
 			c.JSON(
 				http.StatusGatewayTimeout,
 				gin.H{
-					"code":    http.StatusGatewayTimeout,
-					"token":   nil,
-					"message": "Request timed out",
+					"code":          http.StatusGatewayTimeout,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Request timed out",
 				},
 			)
 			return
@@ -141,27 +142,20 @@ func (a *Auth) Login(c *gin.Context) {
 			c.JSON(
 				http.StatusBadRequest,
 				gin.H{
-					"code":    http.StatusBadRequest,
-					"token":   nil,
-					"message": "Invalid credentials",
-				},
-			)
-		case codes.NotFound:
-			c.JSON(
-				http.StatusBadRequest,
-				gin.H{
-					"code":    http.StatusBadRequest,
-					"token":   nil,
-					"message": "App not found",
+					"code":          http.StatusBadRequest,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Invalid credentials",
 				},
 			)
 		case codes.Internal:
 			c.JSON(
 				http.StatusInternalServerError,
 				gin.H{
-					"code":    http.StatusInternalServerError,
-					"token":   nil,
-					"message": "Internal error",
+					"code":          http.StatusInternalServerError,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Internal error",
 				},
 			)
 		}
@@ -170,9 +164,10 @@ func (a *Auth) Login(c *gin.Context) {
 	c.JSON(
 		http.StatusOK,
 		gin.H{
-			"code":    http.StatusCreated,
-			"token":   resp.Token,
-			"message": "Successful login user",
+			"code":          http.StatusCreated,
+			"access_token":  resp.AccessToken,
+			"refresh_token": resp.RefreshToken,
+			"message":       "Successful login user",
 		},
 	)
 }
@@ -239,6 +234,137 @@ func (a *Auth) IsAdmin(c *gin.Context) {
 			"code":    http.StatusCreated,
 			"isAdmin": resp.IsAdmin,
 			"message": "Successful user verification for admin",
+		},
+	)
+}
+
+func (a *Auth) Refresh(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), a.config.Timeout)
+	defer cancel()
+
+	var req pb.RefreshRequest
+
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"code":          http.StatusBadRequest,
+				"access_token":  nil,
+				"refresh_token": nil,
+				"message":       "JSON is incorrect",
+			},
+		)
+		return
+	}
+	
+	resp, err := a.authClient.Refresh(ctx, &req)
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			c.JSON(
+				http.StatusGatewayTimeout,
+				gin.H{
+					"code":          http.StatusGatewayTimeout,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Request timed out",
+				},
+			)
+			return
+		}
+		st, _ := status.FromError(err)
+		code := st.Code()
+		switch code {
+		case codes.InvalidArgument:
+			c.JSON(
+				http.StatusUnauthorized,
+				gin.H{
+					"code":          http.StatusUnauthorized,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Invalid refresh token",
+				},
+			)
+		case codes.Internal:
+			c.JSON(
+				http.StatusInternalServerError,
+				gin.H{
+					"code":          http.StatusInternalServerError,
+					"access_token":  nil,
+					"refresh_token": nil,
+					"message":       "Internal error",
+				},
+			)
+		}
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+			"code":          http.StatusOK,
+			"access_token":  resp.AccessToken,
+			"refresh_token": resp.RefreshToken,
+			"message":       "Successfully refresh tokens",
+		},
+	)
+}
+
+func (a *Auth) Logout(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), a.config.Timeout)
+	defer cancel()
+
+	var req pb.LogoutRequest
+
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"code":    http.StatusBadRequest,
+				"message": "JSON is incorrect",
+			},
+		)
+		return
+	}
+	_, err = a.authClient.Logout(ctx, &req)
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			c.JSON(
+				http.StatusGatewayTimeout,
+				gin.H{
+					"code":    http.StatusGatewayTimeout,
+					"message": "Request timed out",
+				},
+			)
+			return
+		}
+		st, _ := status.FromError(err)
+		code := st.Code()
+		switch code {
+		case codes.InvalidArgument:
+			c.JSON(
+				http.StatusUnauthorized,
+				gin.H{
+					"code":    http.StatusUnauthorized,
+					"message": "Invalid refresh token",
+				},
+			)
+		case codes.Internal:
+			c.JSON(
+				http.StatusInternalServerError,
+				gin.H{
+					"code":    http.StatusInternalServerError,
+					"message": "Internal error",
+				},
+			)
+		}
+		return
+	}
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+			"code":    http.StatusOK,
+			"message": "Successfully logout user",
 		},
 	)
 }
